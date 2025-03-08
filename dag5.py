8d1a3fb605aea95f8a78f8ed11b6a744f449f914
Suraj Kumar Kedia <80747203+surajkd15@users.noreply.github.com>
2025-03-08 23:43:43 +0530

    Add files via upload

---
+++ dag5.py
@@ -0,0 +1,25 @@
+import datetime
+import json
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+##from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
+file_path='/home/airflow/gcs/data/config/config_{{env_name}}.json'
+with open(file_path, 'r') as file:
+    data=json.load(file)
+    dataset1=data['dataset']
+
+with DAG(
+    dag_id="practice_5",
+    schedule=None,
+    user_defined_macros={"dataset": dataset1},
+):
+   
+    def run_sql():
+        opr_call_sproc1 = BigQueryOperator(
+            task_id="example", 
+            sql = """update_query.sql""",
+            use_legacy_sql=False,
+        )
+
+    run_sql()
