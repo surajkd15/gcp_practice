41e10627559e493833be44d1192a4253347654d1
Suraj Kumar Kedia <80747203+surajkd15@users.noreply.github.com>
2025-03-08 23:52:21 +0530

    Update dag5.py

--- dag5.py
+++ dag5.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 ##from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
-file_path='/home/airflow/gcs/data/config/config_{{env_name}}.json'
+file_path='/home/airflow/gcs/data/config/config_${env_name}.json'
 with open(file_path, 'r') as file:
     data=json.load(file)
     dataset1=data['dataset']
